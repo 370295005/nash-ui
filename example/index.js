5999604d8f22c7360bb2884eb02fec76bb56d91f
370295005 <370295005@qq.com>
2022-10-12 20:29:27 +0800

    feat(components): 修改示例引入组件的方式

--- example/index.js
+++ example/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
-import NashUI from '@/index.js'
+import NashUI from '../lib/index.js'
 import ContentContainer from './components/ContentContainer'
 import './assets/css/reset.css'
+import '../lib/index.min.css'
 Vue.component('ContentContainer', ContentContainer)
 Vue.use(NashUI)
 const vm = new Vue({
