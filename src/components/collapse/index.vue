73e9bd709f098443e9660ade550d4a7da1ddb329
Nash <370295005@qq.com>
2022-10-13 17:00:11 +0800

    chore(other): 修改打包配置

--- src/components/collapse/index.vue
+++ src/components/collapse/index.vue
@@ -1,2 +1,19 @@
 <!-- 折叠面板 -->
 <!-- https://vant-contrib.gitee.io/vant/#/zh-CN/collapse -->
+<template>
+  <div class="nash-collapse">
+    <div class="title"></div>
+  </div>
+</template>
+<script>
+export default {
+  model: {
+    prop: 'isActive',
+    event: 'toggle'
+  },
+  props: {
+    // 是否为手风琴模式
+    accordion: Boolean
+  }
+}
+</script>
